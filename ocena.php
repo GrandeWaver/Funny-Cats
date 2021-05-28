538d5e076d806fd3baadff447a1dea497587643d
Waver <68419080+GrandeWaver@users.noreply.github.com>
2021-05-28 14:03:23 +0200

    Add files via upload

---
+++ ocena.php
@@ -0,0 +1,54 @@
+<?php
+
+if(isset($_GET["lubienie"]))
+{
+  require_once 'dbconnect.php';
+  $polaczenie = new mysqli($host, $user, $password);
+
+  if ($polaczenie->connect_errno != 0)
+  {
+    echo "Error: ".$polaczenie->connect_errno." Opis: ".$polaczenie->connect_error;
+  }
+  else
+  {
+  mysqli_query($polaczenie, "SET CHARSET utf8");
+  mysqli_query($polaczenie, "SET NAMES 'utf8' COLLATE 'utf8_polish_ci'");
+
+  mysqli_select_db($polaczenie, $database);
+
+  $polubienia = json_decode($_GET['ilosc'], true);
+  $id = json_decode($_GET['id'], true);
+  $polubienia = $polubienia + 1;
+  $polaczenie->query("UPDATE filmiki SET polubienia = $polubienia WHERE id='$id'");
+  echo "wszystko git";
+
+  $polaczenie->close();
+  }
+}
+
+if(isset($_GET["nielubienie"]))
+{
+  require_once 'dbconnect.php';
+  $polaczenie = new mysqli($host, $user, $password);
+
+  if ($polaczenie->connect_errno != 0)
+  {
+    echo "Error: ".$polaczenie->connect_errno." Opis: ".$polaczenie->connect_error;
+  }
+  else
+  {
+  mysqli_query($polaczenie, "SET CHARSET utf8");
+  mysqli_query($polaczenie, "SET NAMES 'utf8' COLLATE 'utf8_polish_ci'");
+
+  mysqli_select_db($polaczenie, $database);
+
+  $nielubienia = json_decode($_GET['ilosc'], true);
+  $id = json_decode($_GET['id'], true);
+  $nielubienia = $nielubienia + 1;
+  $polaczenie->query("UPDATE filmiki SET nielubienia = $nielubienia WHERE id='$id'");
+  echo "wszystko git";
+
+  $polaczenie->close();
+  }
+}
+?>
